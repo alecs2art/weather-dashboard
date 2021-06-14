5607858325483de071bad007e1d6cb2fed0dce1d
unknown <alecs2art@gmail.com>
2021-06-13 20:50:01 -0700

    minimal html, working in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var apiKey = "e196ee372d18ba4e4a4e491fffa2b30c"
+
+var submitBtn = document.getElementById("btn");
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+}
+
+}
