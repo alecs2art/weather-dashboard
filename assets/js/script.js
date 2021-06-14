279e131370c620e2ff7396ebb6d5686e1a9b84ed
unknown <alecs2art@gmail.com>
2021-06-13 22:55:50 -0700

    wrote code to fetch...not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 var apiKey = "e196ee372d18ba4e4a4e491fffa2b30c"
-
 var submitBtn = document.getElementById("btn");
+var searchCity = document.getElementById("sForm");
 
-submitBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-}
+submitBtn.addEventListener("click", function() {
 
-}
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=e196ee372d18ba4e4a4e491fffa2b30c")
+    
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    
+});
+
