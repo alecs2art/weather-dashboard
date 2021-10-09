d02124c06c533099940d1577eb876db6be352bc9
unknown <alecs2art@gmail.com>
2021-10-09 14:56:07 -0700

    added some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,19 @@ console.log(searchCity.value);
 
 submitButton.addEventListener('click', function() {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchCity.value+'&appid=e196ee372d18ba4e4a4e491fffa2b30c')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchCity.value+'&appid=e196ee372d18ba4e4a4e491fffa2b30c')   
         .then(res => res.json())
         .then(data => {
             var cityData = data['name'];
             var tempData = data['main']['temp'];
-            var windData = data['wind'];
+            var windData = data['wind']['speed'];
             var humidityData = data['main']['humidity'];
 
             searchCity.innerHTML = cityData;
             temp.innerHTML = 'Tempature:' + tempData;
-            wind.innerHTML = 'Wind:' + windData;
+            wind.innerHTML = 'Wind Speed:' + windData;
             humidity.innerHTML = 'Humidity:' + humidityData;
+            console.log(data);
         });
         
 });
